a048e32992fd4036b184dfd2864fea0fbe7fdd19
linroex <linroex@coder.tw>
2015-01-05 15:56:19 +0800

    marked update

--- app/views/head.blade.php
+++ app/views/head.blade.php
@@ -9,10 +9,29 @@
 <script src="{{url('scripts/vendor/modernizr.js')}}"></script>
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 <script src="{{url('bower_components/marked/lib/marked.js')}}"></script>
+
+<style>
+    .issue-main-content h1,h2,h3,h4,h5,h6{
+        margin-top: 10px;
+    }
+    .issue-main-content ul li{
+        list-style: disc;
+    }
+    .issue-main-content ol li{
+        list-style: decimal;
+    }
+    
+    .issue-main-content ul,.issue-main-content ol{
+        padding-left: 1cm;
+    }
+
+</style>
+    
 <script>
     
     $(document).ready(function(){
         $('.issue-main-content').html(marked($('.issue-main-content').text().trim(),{breaks:true}));
+        $('.issue-main-content a').attr('target','_blank');
         // $('.issue-main-content').html($('.issue-main-content').html().replace(/\n/g,"<br/>"));
     })
 </script>
