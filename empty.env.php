5c77e6b38ab5857413f71cf152c7a8eb089a8d9c
Inndy <Inndy.mail@gmail.com>
2015-01-07 14:41:56 +0800

    Add template for .env.php

---
+++ empty.env.php
@@ -0,0 +1,12 @@
+<?php
+    return [
+        'smtp.user'           => 'no-reply@uvote.cc',
+        'smtp.passwd'         => '',
+        'UVOTE_DB_HOST'       => '127.0.0.1',
+        'UVOTE_DB_NAME'       => 'uvote',
+        'UVOTE_DB_USERNAME'   => 'uvote',
+        'UVOTE_DB_PASSWD'     => '',
+        'UVOTE_FB_APP_ID'     => '',
+        'UVOTE_FB_APP_SECRET' => '',
+        'google_analytics'    => ''
+    ];
