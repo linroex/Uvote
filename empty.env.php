71a1a9b9d65ca1f74f49bb402fe0721a47576f64
Inndy <Inndy.mail@gmail.com>
2015-01-09 00:07:41 +0800

    Add comment to .env.php template

--- empty.env.php
+++ empty.env.php
@@ -1,12 +1,19 @@
 <?php
     return [
+        // SMTP settings
         'smtp.user'           => 'no-reply@uvote.cc',
         'smtp.passwd'         => '',
+
+        // MySQL database settings
         'UVOTE_DB_HOST'       => '127.0.0.1',
         'UVOTE_DB_NAME'       => 'uvote',
         'UVOTE_DB_USERNAME'   => 'uvote',
         'UVOTE_DB_PASSWD'     => '',
+
+        // Facebook API settings
         'UVOTE_FB_APP_ID'     => '',
         'UVOTE_FB_APP_SECRET' => '',
+
+        // Google Analytics settings
         'google_analytics'    => ''
     ];
