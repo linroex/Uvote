76fdb074160ab2369b75d2e73a9c870b18d9adb2
Inndy <Inndy.mail@gmail.com>
2015-01-09 00:25:24 +0800

    Add sqlite database configure

--- app/config/local/database.php
+++ app/config/local/database.php
@@ -22,6 +22,12 @@ return array(
 
 	'connections' => array(
 
+		'sqlite' => array(
+			'driver'   => 'sqlite',
+			'database' => __DIR__.'/../../database/development.sqlite',
+			'prefix'   => '',
+		),
+
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
