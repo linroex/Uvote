99346cdc9966930745503846b191ec64c292106b
Inndy <Inndy.mail@gmail.com>
2015-01-09 14:48:22 +0800

    Default value for Issue status

--- app/models/Issues.php
+++ app/models/Issues.php
@@ -3,6 +3,7 @@
 class Issues extends Eloquent{
     protected $table = 'issues';
     protected $guarded = ['id'];
+    protected $attributes = [ 'status' => 1 ];
     public $primaryKey = 'id';
     public $timestamps = true;
 }
